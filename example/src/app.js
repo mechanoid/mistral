0c69d9cba981d59f3bbea06d62c9228f3a62e3f8
Falk Hoppe <falkhoppe81@gmail.com>
2016-11-04 20:31:36 +0100

    Updates

--- example/src/app.js
+++
@@ -1,7 +0,0 @@
-/* global requirejs */
-
-requirejs({ baseUrl: 'dist' }, ['components/button', 'util/logger'], (Button, logger) => {
-  const button = new Button('button')
-  button.init()
-  logger(button)
-})
