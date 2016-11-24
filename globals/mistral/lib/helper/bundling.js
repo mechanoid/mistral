25a503526d158201e1f5c8a08d8c430edde443c4
Falk Hoppe <falkhoppe81@gmail.com>
2016-11-24 11:03:38 +0100

    Updates

--- globals/mistral/lib/helper/bundling.js
+++ globals/mistral/lib/helper/bundling.js
@@ -1,28 +1,31 @@
 const path = require('path')
-const denodeify = require('denodeify')
-const glob = denodeify(require('glob'))
 
-const prefixGlobPattern = (prefix, globPattern) => `${prefix}/${globPattern}`
-
+// file basename without js-extension
 const jsBaseName = (jsFilePath) => path.basename(jsFilePath, '.js')
 
+// the direct parent folder name
 const folderName = (filePath) => path.basename(path.dirname(filePath))
 
 const isIndexFile = (libPath) => jsBaseName(libPath) === 'index'
 
+// lib names are usually build by file name. For files named index
+// the parent folder name is used instead.
 const libName = (libPath) =>
   isIndexFile(libPath) ? folderName(libPath) : jsBaseName(libPath)
 
-const oneFolderUp = (filePath) => {
+// for module path resolution we don't want to have the relative dot path for dir names
+const cleanedDirName = (filePath) => {
   const dirName = path.dirname(filePath)
   return dirName === '.' ? '' : dirName
 }
 
+// module base path is the target folder the actual module file will be located in
+// and is relative to the base path.
 const moduleBasePath = (basePath, libPath) => {
   const libFolder = path.dirname(libPath)
   const pathDifference = path.relative(basePath, libFolder)
 
-  return (isIndexFile(libPath)) ? oneFolderUp(pathDifference) : pathDifference
+  return (isIndexFile(libPath)) ? cleanedDirName(pathDifference) : pathDifference
 }
 
 module.exports = (config) => ({
@@ -32,12 +35,13 @@ module.exports = (config) => ({
   folderName,
   moduleBasePath,
 
-  collectLibs: (bundle) => glob(prefixGlobPattern(config.base, bundle)),
+  // module file path for the bundling target
+  modulePath: (libPath) => path.join(
+    config.paths.dist,
+    moduleBasePath(config.base, libPath),
+    `${libName(libPath)}.js`),
 
-  modulePath: (libPath) => path.resolve(config.paths.dist, moduleBasePath(config.base, libPath), `${libName(libPath)}.js`),
-
-  flattenBundles: (globs) => globs.reduce((bundles, bundle) => bundles.concat(bundle), []),
-
+  // module bundles considered as externals for the bundle process
   externals: (bundles) => bundles.map((libPath) => {
     const moduleBase = moduleBasePath(config.base, libPath)
 
